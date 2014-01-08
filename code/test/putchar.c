d596ffabfab473069c32b8971a58a8ef53f909ce
Guiroux Hugo <gx.hugo@gmail.com>
2014-01-08 10:01:54 +0100

    Test: putchar.c (given example step 2)
    For now override PutChar. Need to be defined as syscall later.

---
+++ code/test/putchar.c
@@ -0,0 +1,24 @@
+#include "syscall.h"
+
+/**
+ * Provided code from step 2
+ **/
+
+// For now define PutChar to compile (will be syscall later)
+#define PutChar(x)
+void print(char c, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        PutChar(c+i);
+    }
+
+    PutChar('\n');
+}
+
+int main()
+{
+    print('a',4);
+    Halt();
+}
