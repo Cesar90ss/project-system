06785a43183ce0013ec60d52a36574cb549a3bb8
Guiroux Hugo <gx.hugo@gmail.com>
2014-01-08 23:40:03 +0100

    Mips: Handle return code for user program.

--- code/test/matmult.c
+++ code/test/matmult.c
@@ -35,5 +35,5 @@ main ()
 	    for (k = 0; k < Dim; k++)
 		C[i][j] += A[i][k] * B[k][j];
 
-    Exit (C[Dim - 1][Dim - 1]);	/* and then we're done */
+    return C[Dim - 1][Dim - 1];	/* and then we're done */
 }
