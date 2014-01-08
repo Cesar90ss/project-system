c98363613dced9b337f0347866ce85bfad6fd915
3kinox <rien@quedalle.com>
2014-01-08 13:40:06 +0100

    Implement Synchconsole and makefile entry for step2

--- code/machine/synchconsole.cc
+++ code/machine/synchconsole.cc
@@ -3,46 +3,53 @@
 #include "synchconsole.h"
 #include "synch.h"
 
-//static Semaphore *readAvail;
-//static Semaphore *writeDone;
+static Semaphore *SynchConsole_readAvail;
+static Semaphore *SynchConsole_writeDone;
 
-//static void ReadAvail(int arg) { readAvail->V(); }
-//static void WriteDone(int arg) { writeDone->V(); }
+static void SynchConsole_ReadAvail(int arg) { SynchConsole_readAvail->V(); }
+static void SynchConsole_WriteDone(int arg) { SynchConsole_writeDone->V(); }
 
 SynchConsole::SynchConsole(char *readFile, char *writeFile)
 {
-  //readAvail = new Semaphore("read avail", 0);
-  //writeDone = new Semaphore("write done", 0);
-  console(readFile, writeFile, readAvail, writeDone, 0)
+  SynchConsole_readAvail = new Semaphore("SynchConsole_read avail", 0);
+  SynchConsole_writeDone = new Semaphore("SynchConsole_write done", 0);
+  console = new Console(readFile, writeFile, SynchConsole_ReadAvail, SynchConsole_WriteDone, 0);
 }
 
 SynchConsole::~SynchConsole()
 {
-//delete console;
-//delete writeDone;
-//delete readAvail;
-
+delete console;
+delete SynchConsole_writeDone;
+delete SynchConsole_readAvail;
 }
 
 void SynchConsole::SynchPutChar(const char ch)
 {
   console->PutChar(ch);
-  writeDone->P(); //wait for the end of the writing
+  SynchConsole_writeDone->P(); //wait for the end of the writing
 }
 
 
 char SynchConsole::SynchGetChar()
 {
-  readAvail->P();
+  SynchConsole_readAvail->P();
   return console->GetChar();
 }
 
 void SynchConsole::SynchPutString(const char s[])
 {
-// ...
+  int i = 0;
+  while( s[i] != '\0' )
+  {
+    SynchPutChar(s[i]);
+  }
 }
 
 void SynchConsole::SynchGetString(char *s, int n)
 {
-// ...
+  int i;
+  for(i=0; i<n;i++)
+  {
+    s[i] = SynchGetChar();
+  }
 }
