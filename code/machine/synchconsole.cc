ce46b6d93773634fff16810122ce37518a7c6622
Guiroux Hugo <gx.hugo@gmail.com>
2014-01-09 16:42:35 +0100

    SynchConsole: Thread-safety.
    Adding monitor in function to prevent concurrent issues.

--- code/machine/synchconsole.cc
+++ code/machine/synchconsole.cc
@@ -1,7 +1,6 @@
 #include "copyright.h"
 #include "system.h"
 #include "synchconsole.h"
-#include "synch.h"
 #include "utility.h"
 #include <cmath>
 #include <cctype>
@@ -19,6 +18,7 @@ SynchConsole::SynchConsole(char *readFile, char *writeFile)
     SynchConsole_readAvail = new Semaphore("SynchConsole_read avail", 0);
     SynchConsole_writeDone = new Semaphore("SynchConsole_write done", 0);
     console = new Console(readFile, writeFile, SynchConsole_ReadAvail, SynchConsole_WriteDone, 0);
+    monitor = new Semaphore("SynchConsole internal monitor", 1);
 }
 
 SynchConsole::~SynchConsole()
@@ -26,47 +26,71 @@ SynchConsole::~SynchConsole()
     delete console;
     delete SynchConsole_writeDone;
     delete SynchConsole_readAvail;
+    delete monitor;
 }
 
-void SynchConsole::SynchPutChar(const char ch)
+
+void SynchConsole::__PutChar(const char ch)
 {
     console->PutChar(ch);
     //wait for the end of the writing
     SynchConsole_writeDone->P();
 }
 
-int SynchConsole::SynchGetChar()
+void SynchConsole::SynchPutChar(const char ch)
+{
+    monitor->P();
+    __PutChar(ch);
+    monitor->V();
+}
+
+int SynchConsole::__GetChar()
 {
     //wait for avalaible character
     SynchConsole_readAvail->P();
     return console->GetChar();
 }
 
+int SynchConsole::SynchGetChar()
+{
+    monitor->P();
+    int res = __GetChar();
+    monitor->V();
+    return res;
+}
+
 void SynchConsole::SynchPutString(const char s[])
 {
+    monitor->P();
+
     int i = 0;
     //simulate strlen
     while (s[i] != '\0')
-        SynchPutChar(s[i++]);
+        __PutChar(s[i++]);
+
+    monitor->V();
 }
 
 char *SynchConsole::SynchGetString(char *s, int n)
 {
+    monitor->P();
+
     int i;
     //try to get n-1 characters
     for(i = 0; i < n - 1; i++)
     {
-        s[i] = SynchGetChar();
+        s[i] = __GetChar();
         if (console->isEOF())
-	{
-	    s[i] = '\0';
-	    return NULL;
-	   
-	}
+        {
+            s[i] = '\0';
+            monitor->V();
+            return NULL;
+        }
     }
 
     //add end of string
     s[i] = '\0';
+    monitor->V();
     return s;
 }
 
@@ -81,7 +105,9 @@ void SynchConsole::SynchPutInt(int i)
 
 int SynchConsole::SynchGetInt()
 {
-    char c = SynchGetChar();
+    monitor->P();
+
+    char c = __GetChar();
     char buf[MAX_STRING_SIZE + 1];
     int count = 0;
     int num = -1;
@@ -90,9 +116,12 @@ int SynchConsole::SynchGetInt()
     while (count < MAX_STRING_SIZE && !isspace(c))
     {
         buf[count++] = c;
-        c = SynchGetChar();
+        c = __GetChar();
     }
 
     sscanf(buf, "%d", &num);
+
+    monitor->V();
+
     return num;
 }
