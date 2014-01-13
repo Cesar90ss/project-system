67f5fdf39fd80bc482448016414be371f86e1d0e
sloanlost <sloan_lost@hotmail.com>
2014-01-13 15:50:51 +0000

    Regression test : fixed Exit_Catch_Code & return_Catch_Code.sh.

--- code/regression/Exit_Catch_Code.sh
+++ code/regression/Exit_Catch_Code.sh
@@ -1,7 +1,7 @@
 NACHOS_DIR=../build/
 
 cd $NACHOS_DIR
-OUTPUT=$(./nachos-step2 -x test_exit_1 -d a| grep "Exit program, return code exit(1)")
+OUTPUT=$(./nachos-step2 -x test_exit_1 -d m| grep "Exit program, return code exit(1)")
 
 if [ -n "$OUTPUT" ]; then
     exit 0
