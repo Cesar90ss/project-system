6d96f2d995368332134631c50c43a57f307f0561
3kinox <fafangi@gmail.com>
2014-01-14 10:46:47 +0100

    modify test for argument(does not work for now, will have to fix after next pull)

--- code/test/threadcreate.c
+++ code/test/threadcreate.c
@@ -7,6 +7,9 @@
 void fun(void* arg)
 {
 	PutInt(7);
+	int *i = (int*) arg;
+	//for(i=0; i< 100; i++);
+	PutInt(*i);
 	UserThreadExit();
 }
 
@@ -18,5 +21,10 @@ int main()
 	PutInt(5);
     for (i = 1; i < 10000; i++);
 
+int *arg = (int *)544;
+	*arg = 26;
+	UserThreadCreate(&fun, arg);
+	PutInt((int)arg);
+	PutInt(9);
 	return 0;
 }
