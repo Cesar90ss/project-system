2804f4c8ea827027d937a0799910d9ba17f2d17f
sloanlost <sloan_lost@hotmail.com>
2014-01-14 17:01:39 +0000

    Regression test : testing join on simple thread programs. And fixed threadJoinSimple.c

---
+++ code/regression/threadJoinSimple.sh
@@ -0,0 +1,14 @@
+NACHOS_DIR=../build/
+
+cd $NACHOS_DIR
+
+RESULT_STRING="0123456789joined successfull"
+
+OUTPUT=$(./nachos-step3 -rs 2 -x threadJoinSimple | grep "$RESULT_STRING")
+
+if [ -n "$OUTPUT" ]; then
+    exit 0
+else
+    exit -1
+fi
+
--- code/test/threadJoinSimple.c
+++ code/test/threadJoinSimple.c
@@ -3,7 +3,9 @@
 void fun(void* arg)
 {
 	int i;
-	for(i=0;i<10000;i++);
+	for(i=0; i<10; i++){
+        PutInt(i);
+    }
 	UserThreadExit(0);
 }
 
@@ -11,13 +13,21 @@ int main()
 {
 	PutString("Parent start\n");
 	int tid = UserThreadCreate(&fun, 0);
+
+    if(tid == -1)
+    {
+        PutString("Error while creating thread\n");
+        return -1;
+    }
+
 	if(UserThreadJoin(tid, 0) == -1)
 	{
 		PutString("joined failed !\n");
+        return -1;
 	}
 	else
 	{
-		PutString("joined successful\n");
+		PutString("joined successfull\n");
 	}
 	PutString("Parent finish\n");
 	return 0;
