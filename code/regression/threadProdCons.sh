19fc4b1d69dca5323c39c14d7e675927c85b6b03
lucas <lucas.felix@hotmail.fr>
2014-01-15 13:38:43 +0100

    Test: add the producer consumer test with semaphores

---
+++ code/regression/threadProdCons.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+NACHOS_DIR=../build/
+
+cd $NACHOS_DIR
+OUTPUT=`./nachos-step3 -rs 4 -x threadProdCons | head -n -10`
+M=$(echo "$OUTPUT" | sort | uniq -c | uniq -w 1)
+if [ $( echo $M | wc -l ) -ne "1" ]; then
+	exit -1
+fi
+
+
+M=$(echo $M | head -c 1)
+
+if [ $M -eq "2" ]; then
+	exit 0
+else
+	exit -2
+fi
