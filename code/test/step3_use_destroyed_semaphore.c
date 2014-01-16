ba23f409a3ce84963f9c7eebc108236480f2e90d
lucas <lucas.felix@hotmail.fr>
2014-01-16 11:12:13 +0100

    Test: change the message of step3_use_destroyed_semaphore.c, should print error when there is no error because we wait for an error...

--- code/test/step3_use_destroyed_semaphore.c
+++ code/test/step3_use_destroyed_semaphore.c
@@ -5,7 +5,7 @@ int main()
 {
 	int sem = UserSemaphoreCreate("sem",1);// create semaphore
 	UserSemaphoreDestroy(sem); //destroy main thread
-	if((UserSemaphoreP(sem) == -1) || (UserSemaphoreV(sem) == -1))
+	if((UserSemaphoreP(sem) != -1) || (UserSemaphoreV(sem) != -1))
 	{
 	    PutString("error \n");
 	}
