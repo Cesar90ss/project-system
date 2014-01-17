a79b15f2808c172995b34a89b0652c898db9fe6b
sloanlost <sloan_lost@hotmail.com>
2014-01-17 15:20:35 +0000

    Regression test : fixed step3_thread_Multiple_Kill_Create which was always working (even when it shouldnt).

--- code/regression/step3_thread_Multiple_Kill_Create.sh
+++ code/regression/step3_thread_Multiple_Kill_Create.sh
@@ -2,12 +2,12 @@ NACHOS_DIR=../build/
 
 cd $NACHOS_DIR
 
-RESULT_STRING="Error while creating thread"
+RESULT_STRING="Parent finish"
 
 OUTPUT=$(./nachos-step4 -rs 2 -x thread_Multiple_Kill_Create | grep -P "$RESULT_STRING")
 
 if [ -n "$OUTPUT" ]; then
-    exit -1
-else
     exit 0
+else
+    exit -1
 fi 
