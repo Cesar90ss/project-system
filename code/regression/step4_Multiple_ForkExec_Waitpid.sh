b151d50d23c38cc7871d09c7b7a88c1ad2571f33
sloanlost <sloan_lost@hotmail.com>
2014-01-21 15:28:34 +0000

    Regression tests : fixing step4_Multiple_ForkExec_Waitpid.sh which was passing regresssion even with Exit(0) at the start of the main

--- code/regression/step4_Multiple_ForkExec_Waitpid.sh
+++ code/regression/step4_Multiple_ForkExec_Waitpid.sh
@@ -2,16 +2,13 @@ NACHOS_DIR=../build/
 
 cd $NACHOS_DIR
 
-RESULT_STRING="in my world the parent should come after the child I am the child"
-
-OUTPUT=$(./nachos-step4 -rs 0 -x step4_Multiple_ForkExec_Waitpid)
-
-OUTPUT1=$(echo $OUTPUT | grep "$RESULT_STRING")
+RESULT_STRING="in my world the parent should come after the child Machine halting!"
 
 
+OUTPUT=$(./nachos-step4 -rs 0 -x step4_Multiple_ForkExec_Waitpid | grep "$RESULT_STRING")
 
-if [ -n "$OUTPUT1" ]; then
-    exit -1
-else
+if [ -n "$OUTPUT" ]; then
     exit 0
+else
+    exit -1
 fi
