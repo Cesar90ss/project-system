d047918b1931c0e99a840bdcd4c8634e80e69afc
mkmufida <mkmufida@yahoo.com>
2014-01-21 16:25:56 +0100

    Make regression Test for the ForkExec created on itself and explain Step4.org

---
+++ code/regression/step4_ForkExecOnItself.sh
@@ -0,0 +1,24 @@
+NACHOS_DIR=../build/
+
+cd $NACHOS_DIR
+
+
+
+RESULT_STRING1="Error"			# Determine maximum thread created in the same time
+RESULT_STRING2="Hello Myself"		# There is something thats execute
+RESULT_STRING3="Machine halting!"	# No Segmentation fault
+
+OUTPUT=$(./nachos-step4 -x step4_ForkExecOnItself -rs 3)
+
+OUTPUT1=$(echo $OUTPUT | grep "$RESULT_STRING1")
+
+OUTPUT2=$(echo $OUTPUT | grep "$RESULT_STRING2")
+
+OUTPUT3=$(echo $OUTPUT | grep "$RESULT_STRING3")
+
+
+if [ -n "$OUTPUT1" ] && [ -n "$OUTPUT2" ] && [ -n "$OUTPUT3" ]; then
+    exit 0
+else
+    exit -1
+fi
