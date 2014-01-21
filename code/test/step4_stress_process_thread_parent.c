98e0206fb8f04f9df3e7d2eb626e4236f0f8c0b7
lucas <lucas.felix@hotmail.fr>
2014-01-21 14:42:31 +0100

    Test: regroup the tests which overtake the process and the thread limits in one

--- code/test/step4_stress_process_thread_parent.c
+++ code/test/step4_stress_process_thread_parent.c
@@ -6,11 +6,11 @@ int main()
 	int i;
 	for(i=0;i<NB_PROC;i++)
 	{
-		if(ForkExec("../build/step4_multiple_Thread_Process") >= 0)
+		if(ForkExec("../build/step4_stress_process_thread_child") >= 0)
 			PutString("Process Creation successfull\n");
 		else
 			PutString("Process creation failed\n");
 	}
-	PutString("Parent ending\n");
+	PutString("Parent end\n");
 	return 0;
 }
