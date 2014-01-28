9ccfcb7c5203e2cacd932b66d1bb3ed037572af0
sloanlost <sloan_lost@hotmail.com>
2014-01-28 11:24:54 +0000

    Fixing some test and adding a missing .desc

--- code/regression/step5_too_big_file.sh
+++ code/regression/step5_too_big_file.sh
@@ -8,13 +8,14 @@ cd $NACHOS_DIR
 #create 200K file in /tmp
 rm /tmp/test
 dd if=/dev/zero of=/tmp/test bs=200K count=1
+./nachos-step5 -md a
 
 #create file structure
 ./nachos-step5 -cp /tmp/test test
 
 # Test case
 OUTPUT=$(./nachos-step5 -l / | head -n -10 | tr "\n" "|")
-EXPECTED_OUTPUT=""
+EXPECTED_OUTPUT="d - a|"
 
 if [ "$OUTPUT" = "$EXPECTED_OUTPUT" ]; then
     RETURN=0
