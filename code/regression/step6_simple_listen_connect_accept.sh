907a3a75a82ead3881f00224e3cdcc30680c3deb
mkmufida <mkmufida@yahoo.com>
2014-01-29 13:48:20 +0100

    Modified the regression simple test of connect and accept

--- code/regression/step6_simple_listen_connect_accept.sh
+++ code/regression/step6_simple_listen_connect_accept.sh
@@ -1,11 +1,15 @@
 NACHOS_DIR=../build/
 
 cd $NACHOS_DIR
-OUTPUT=`./nachos-step6 -m 0 -x step6_test_simple_listen_accept & ./nachos-step6 -m 1 -x step6_test_simple_listen_connect`
 
-if [ -n "$(echo "$OUTPUT" | grep "Received message : Got it!")" ] && [ -n "$(echo "$OUTPUT" | grep "Received message : Hello there!")" ]; then	
+for i in {1..5}
+do
+OUTPUT=`./nachos-step6 -m 0 -rs 5 -x step6_test_simple_listen_accept & ./nachos-step6 -m 1 -rs 3 -x step6_test_simple_listen_connect`
+
+if [ -n "$(echo "$OUTPUT" | grep "Received message : Hello there!")" ] && [ -n "$(echo "$OUTPUT" | grep "Received message : Got it!")" ]; then	
 	exit 0 
-else
-	exit -1
 fi
+done
+	exit -1
+
 
