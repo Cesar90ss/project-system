7e06a1b2491974152a98cc335e6412f1c97cf7de
mkmufida <mkmufida@yahoo.com>
2014-01-29 14:16:26 +0100

    Handled error in regression test

--- code/regression/step6_simple_listen_connect_accept.sh
+++ code/regression/step6_simple_listen_connect_accept.sh
@@ -1,12 +1,11 @@
 NACHOS_DIR=../build/
 
 cd $NACHOS_DIR
-
-for i in {1..5}
+for i in {1..3}
 do
-OUTPUT=`./nachos-step6 -m 0 -rs 5 -x step6_test_simple_listen_accept & ./nachos-step6 -m 1 -rs 3 -x step6_test_simple_listen_connect`
+OUTPUT=`./nachos-step6 -m 0 -rs 2 -x step6_test_simple_listen_accept & ./nachos-step6 -m 1 -rs 4 -x step6_test_simple_listen_connect`
 
-if [ -n "$(echo "$OUTPUT" | grep "Received message : Hello there!")" ] && [ -n "$(echo "$OUTPUT" | grep "Received message : Got it!")" ]; then	
+if [ -n "$(echo "$OUTPUT" | grep "Received message : Got it!")" ] && [ -n "$(echo "$OUTPUT" | grep "Received message : Hello there!")" ]; then	
 	exit 0 
 fi
 done
