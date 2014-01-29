ffb4288a3a3d0b3cb2091c82df9c0f095519e723
Guiroux Hugo <gx.hugo@gmail.com>
2014-01-29 14:06:36 +0100

    Build: now compile only final

--- code/run_with_valgrind.sh
+++ code/run_with_valgrind.sh
@@ -8,33 +8,21 @@ make -j >/dev/null
 
 # Move step4 & step5 to override
 echo "Move"
-mv build/{,_}nachos-step4
-mv build/{,_}nachos-step5
+mv build/{,_}nachos-final
 
 # Clean previous log
 echo "Clean"
 rm -f build/$VALGRIND_LOG
 
 # Set up hook script
-echo "Hook step4"
+echo "Hook final"
 echo "#!/bin/bash
 echo '=======================================' >> $VALGRIND_LOG
-echo ./nachos-step4 \$@ >> $VALGRIND_LOG
+echo ./nachos-final \$@ >> $VALGRIND_LOG
 echo '=======================================' >> $VALGRIND_LOG
-valgrind --leak-check=full --show-reachable=yes ./_nachos-step4 \$@ 2>>$VALGRIND_LOG
-echo '=======================================' >> $VALGRIND_LOG
-echo >> $VALGRIND_LOG
-" > build/nachos-step4
-chmod +x build/nachos-step4
-
-echo "Hook step5"
-echo "#!/bin/bash
-echo '=======================================' >> $VALGRIND_LOG
-echo ./nachos-step5 \$@ >> $VALGRIND_LOG
-echo '=======================================' >> $VALGRIND_LOG
-valgrind --leak-check=full --show-reachable=yes ./_nachos-step5 \$@ 2>>$VALGRIND_LOG
+valgrind --leak-check=full --show-reachable=yes ./_nachos-final \$@ 2>>$VALGRIND_LOG
 echo '=======================================' >> $VALGRIND_LOG
 echo >> $VALGRIND_LOG
-" > build/nachos-step5
-chmod +x build/nachos-step5
+" > build/nachos-final
+chmod +x build/nachos-final
 
