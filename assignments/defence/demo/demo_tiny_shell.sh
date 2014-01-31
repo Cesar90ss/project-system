6ae235562c756bebe9284961b4285d9f97a38cb2
mkmufida <mkmufida@yahoo.com>
2014-01-31 11:14:08 +0100

    Remark demo tinyshell

--- assignments/defence/demo/demo_tiny_shell.sh
+++ assignments/defence/demo/demo_tiny_shell.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+NACHOS_DIR=../../../code/build/
+
+cd $NACHOS_DIR
+pwd
 ./nachos-final -f
 ./nachos-final -cp getstringeof run3
 ./nachos-final -cp step4_tiny_shell_test_program2 run1
 ./nachos-final -cp step4_tiny_shell_test_program1 run2
 ./nachos-final -cp step4_tiny_shell run
 
-printf "run1 &\nrun2\nfg 0\nrun3\n" | ./nachos-final -x run -rs 2
+./nachos-final -x run -rs 2
