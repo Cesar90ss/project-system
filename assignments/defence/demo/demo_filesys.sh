304c924e2b831a651eb4d39159a7a1a3a748d3f7
sloanlost <sloan_lost@hotmail.com>
2014-01-31 10:14:31 +0000

    Adding test about filesys

--- assignments/defence/demo/demo_filesys.sh
+++ assignments/defence/demo/demo_filesys.sh
@@ -1,30 +1,53 @@
 NACHOS_DIR=../../../code/build/
 cd $NACHOS_DIR
 
-#format disk
-./nachos-final -f
+function r () 
+{
+    echo $@
+    $@ | head -n -10
+    ./nachos-final -lr / | head -n -10
+}
 
 #create file structure
 echo "Clean up system file"
-./nachos-final -f
+r ./nachos-final -f
 read -p "Press [Enter] to continue..."
 
-echo "Creating directory test"
-./nachos-final -md test
-./nachos-final -lr /
-read -p "Press [Enter] to continue..."
+echo -e "\nCreating directory test"
+r ./nachos-final -md test 
+read
 
-echo "Creating directory test in test2"
-./nachos-final -md /test/test2
-./nachos-final -lr /
+echo -e "\nCreating directory test2 in test"
+r ./nachos-final -md /test/test2
+read
 
-echo "Creating directory test in test2"
-./nachos-final -md /test/test2/test3
-./nachos-final -lr /
+echo -e "\nCreating directory test3 in /test/test2"
+r ./nachos-final -md /test/test2/test3
+read
 
-echo "Trying to create a directory with invalid name"
-./nachos-final -md "//d/sq/"
+echo -e "\nCreating directory removeMe in root"
+r ./nachos-final -md /removeMe
+read
 
-echo "Copying Makefile in /test/test2"
-./nachos-final -cp ../Makefile Makefile
-./nachos-final -lr
+echo -e "\nTrying to create a directory with invalid name"
+r ./nachos-final -md "d///sq/"
+read
+
+echo -e "\nCopying Makefile in /test/test2"
+r ./nachos-final -cp ../Makefile /test/test2/Makefile
+read
+
+echo -e "\nRemoving removeMe directory"
+r ./nachos-final -rd /removeMe
+read
+
+
+echo -e "\nCreating a file big file of 200KO"
+dd if=/dev/zero of=/tmp/bigfile bs=200K count=1
+
+echo -e "\nCopying "
+r ./nachos-final -cp /tmp/bigfile /imp
+read
+
+r ./nachos-final -cp TheEnd run
+./nachos-final -x run
