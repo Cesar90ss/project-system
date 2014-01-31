cee7f04dfdd29647713d6e356ae74d84cb3b8d20
Guiroux Hugo <gx.hugo@gmail.com>
2014-01-31 12:40:50 +0100

    Last commit

--- assignments/defence/demo/demo_tiny_shell.sh
+++ assignments/defence/demo/demo_tiny_shell.sh
@@ -3,10 +3,10 @@ NACHOS_DIR=../../../code/build/
 
 cd $NACHOS_DIR
 pwd
-./nachos-final -f
-./nachos-final -cp getstringeof run3
-./nachos-final -cp step4_tiny_shell_test_program2 run1
-./nachos-final -cp step4_tiny_shell_test_program1 run2
-./nachos-final -cp step4_tiny_shell run
+./nachos-final -f > /dev/null
+./nachos-final -cp getstringeof run3 > /dev/null
+./nachos-final -cp step4_tiny_shell_test_program2 run1 > /dev/null
+./nachos-final -cp step4_tiny_shell_test_program1 run2 > /dev/null
+./nachos-final -cp step4_tiny_shell run > /dev/null
 
 ./nachos-final -x run -rs 2
